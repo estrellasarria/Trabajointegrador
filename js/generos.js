01d6689b41d75c7adaab30514528236f65b26b98
Estrella Sarria <estrellasarria@MacBook-Pro-de-Estrella.local>
2023-11-11 23:10:22 -0300

    seccion de generos funcionando

--- js/generos.js
+++ js/generos.js
@@ -1,26 +1,26 @@
-// VINCULAR API EN LA SECCION DE GENEROS 
+// VINCULAR API EN LA SECCION DE GENEROS PELICULAS
 
 let apiKey= "43c74b59045ed8eefa36be7448cda7ac"
-let endpointGeneros= `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`
+let endpointGenerosPelis= `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`
 
 let generosContainer= document.querySelector("#generosContainer")
 
-fetch(endpointGeneros)
+fetch(endpointGenerosPelis)
 .then(function(res){
     return res.json()
 })
 .then(function(data){
     console.log(data)
-
-    let generos=data.generos
+    let generos=data.genres
     let generosHtml= ""
     for (let i=0; i< generos.length; i++){
         let gen= generos[i];
-        generosHtml += `<article><a href="detallesGeneros.html?type=movie&id=${gen.id}">${gen.name}</a></article>`
+        generosHtml += `<article><a href="detallesGeneros.html?type=movie&id=${gen.id}"><button>${gen.name}</button></a></article>`
        
     }
     generosContainer.innerHTML+= generosHtml
 })
 .catch(function(e){
-    console.log('error '+ e)
+    console.log('error'+ e)
 })
+
