1c8a12a946ea0eb004d34b0c71843f26ea0677ef
Estrella Sarria <estrellasarria@MacBook-Pro-de-Estrella.local>
2023-11-14 20:50:35 -0300

    resultados de busqueda terminados, revisas css.

--- js/resultados.js
+++ js/resultados.js
@@ -1,35 +1,58 @@
+apiKey="43c74b59045ed8eefa36be7448cda7ac"
+let queryString= location.search
+let item= new URLSearchParams(queryString)
 
-let apiKey= "43c74b59045ed8eefa36be7448cda7ac"
-let queryString= window.location.search;
-let urlParams= new URLSearchParams(queryString);
-let searchTerm= urlParams.get('q')
-let searchType= urlParams.get('type')
+let searchItem= item.get("q")
+console.log(searchItem);
 
-if (searchTerm && searchType){
-    document.querySelector('#searchTerm').textContent= searchTerm;
-    let endpoint;
-    if (searchType === 'movie') {
-        endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
-    } else if (searchType === 'tv') {
-        endpoint = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${searchTerm}`;
-    }
+let urlMovies= `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchItem}`
 
-    fetch(endpoint)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(data){
-        console.log(data)
+let urlSeries=  `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${searchItem}`
 
-        let resultadosLista= document.querySelector('#resultadoLista')
-        let noResultados= document.querySelector('#noResultados')
+let mainContainer= document.querySelector('#mainSearch')
+let resultadoLista= document.querySelector('#resultadoLista')
+let textoBusqueda= document.querySelector('#textoBusqueda')
+let noResultados= document.querySelector('#noResultados')
+let searchTerm= document.querySelector('#searchTerm')
 
-        if(data.results.length>0){
-         for (let i=0; i < data.results.length; i++){
-            let results= data.results[i]
-            let resultsItem
-         }   
-        }
-    })
+searchTerm.innerHTML+= searchItem
 
+function crearResultados(movies){
+    return `
+    <article>
+    <a>
+        <img src="https://image.tmdb.org/t/p/w342${movies.poster_path}" alt="${movies.title || movies.name}">
+        <h3>${movies.title || movies.name}</h3>
+        <p>${movies.release_date || movies.first_air_date}</p>
+    </a>
+</article>
+    `
 }
+
+function mostrarResultados(data){
+    if (data.results.length === 0){
+        noResultados.style.display="block"
+}   else {
+    textoBusqueda.innerHTML= `Estos son los resultados de tu busqueda para: ` + searchItem
+    for (let i=0 ; i < data.results.length; i++){
+        resultadoLista.innerHTML+= crearResultados(data.results[i])
+    }
+}}
+
+function manejarError(e){
+    console.log("El error es: "+ e)
+}
+
+fetch(urlMovies)
+.then(function(res){
+    return res.json();
+})
+.then(mostrarResultados)
+.catch(manejarError)
+
+fetch(urlSeries)
+.then(function(res){
+    return res.json();
+})
+.then(mostrarResultados)
+.catch(manejarError)
